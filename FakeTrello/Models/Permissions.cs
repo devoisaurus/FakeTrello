60080beef89289755fc7bdd52b5965a94e12d289
Andi DeVois <adevois@gmail.com>
2017-03-04 10:06:38 -0600

    roles & permissions

--- FakeTrello/Models/Permissions.cs
+++ FakeTrello/Models/Permissions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace FakeTrello.Models
 {
     public class Permissions
     {
+        [Key]
+        public int PermissionId { get; set; }
+
+        public string Name { get; set; }
     }
 }
--- FakeTrello/Models/Roles.cs
+++ FakeTrello/Models/Roles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace FakeTrello.Models
 {
     public class Roles
     {
+        [Key]
+        public int RoleId { get; set; }
+
+        public string Name { get; set; }
     }
 }
